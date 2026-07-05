906f51b169b4eb23f9bc13ed098bc230fe92fc8e
apdishaafici01-afk <apdishaafici01@gmail.com>
2026-07-05 12:04:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,20 @@
 import asyncio
 import os
 from pyrogram import Client, filters
-from pytgcalls import PyTgCalls
 
-# Xogta sirta ah
+# Xogta sirta ah ee Kinesis
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 app = Client("music_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-call_py = PyTgCalls(app)
 
 @app.on_message(filters.command("start"))
 async def start_command(client, message):
-    await message.reply_text("👋 Sannad Wacan! Bot-kii si guul leh ayuu u shaqaynayaa.")
+    await message.reply_text("👋 Sannad Wacan! Bot-kii hadda si guul leh ayuu u shaqaynayaa!")
 
 async def main():
     await app.start()
-    await call_py.start()
     print("🚀 Bot-kii si guul leh ayuu u shaqaynayaa 24/7...")
     await asyncio.Event().wait()
 
