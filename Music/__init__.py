60dbe14255cc123affa49d5c72019014abf7477b
apdishaafici01-afk <apdishaafici01@gmail.com>
2026-07-05 12:59:45 +0300

    Update __init__.py

--- Music/__init__.py
+++ Music/__init__.py
@@ -1,1 +1,13 @@
+from pyrogram import Client
+from pytgcalls import PyTgCalls
 
+from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client(
+    "MusicBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+call_py = PyTgCalls(app)
