93cf71ccfc4dff926a9635b1243b7058cbbeb90e
apdishaafici01-afk <apdishaafici01@gmail.com>
2026-07-05 12:29:09 +0300

    Update and rename main.py to bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,19 @@ import asyncio
 import os
 from pyrogram import Client, filters
 
-# Xogta deegaanka rasmiga ah
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("music_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text("👋 Huraay! Hadda si toos ah ayaan u shaqaynayaa, waana kuu jawaabay!")
+async def start_command(client, message):
+    await message.reply_text("👋 Sannad Wacan! Bot-kii hadda si guul leh ayuu u shaqaynayaa!")
 
 async def main():
-    print("🚀 Bot-ku wuxuu ka bilaabanayaa Kinesis...")
     await app.start()
-    print("✅ Bot-kii wuxuu ku xirmay Telegram!")
+    print("🚀 Bot-kii si guul leh ayuu u shaqaynayaa 24/7...")
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
