e0f8d08ae1ebe7a9345c25fd66a14414b6680586
apdishaafici01-afk <apdishaafici01@gmail.com>
2026-07-05 12:08:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
 import asyncio
 import os
 from pyrogram import Client, filters
+from pytgcalls import GroupCallClient
 
-# Xogta sirta ah ee Kinesis
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 app = Client("music_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+# Nooca dev24 wuxuu isticmaalaa GroupCallClient
+call_py = GroupCallClient(app)
+
 @app.on_message(filters.command("start"))
 async def start_command(client, message):
-    await message.reply_text("👋 Sannad Wacan! Bot-kii hadda si guul leh ayuu u shaqaynayaa!")
+    await message.reply_text("👋 Sannad Wacan! Bot-kii si guul leh ayuu u shaqaynayaa.")
 
 async def main():
     await app.start()
