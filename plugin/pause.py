4446ce8aeaf77af4605868910955ab7c041584ef
apdishaafici01-afk <apdishaafici01@gmail.com>
2026-07-06 18:36:46 +0300

    Update pause.py

--- plugin/pause.py
+++ plugin/pause.py
@@ -1,1 +1,9 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command("pause") & filters.group)
+async def pause_cmd(client, message):
+    try:
+        await client.call_py.pause_stream(message.chat.id)
+        await message.reply_text("⏸ Hesta waa la joojiyay.")
+    except Exception as e:
+        await message.reply_text(f"⚠️ Qalad: {e}")
