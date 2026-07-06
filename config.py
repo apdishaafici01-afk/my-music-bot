a9934fc39be00a76f11b8c83722cf7fef98e0a9c
apdishaafici01-afk <apdishaafici01@gmail.com>
2026-07-06 18:07:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-
-STRING_SESSION = os.getenv("STRING_SESSION", "")
-SUDO_USERS = [int(x) for x in os.getenv("SUDO_USERS", "").split() if x]
-
-BOT_NAME = os.getenv("BOT_NAME", "Music Bot")
+API_ID = int(os.environ.get("API_ID", "0"))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
