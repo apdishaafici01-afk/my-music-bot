676e8b33e96fcdceedc48afad7a6f70a28276649
apdishaafici01-afk <apdishaafici01@gmail.com>
2026-07-06 18:37:07 +0300

    Update skip.py

--- plugin/skip.py
+++ plugin/skip.py
@@ -1,1 +1,9 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command("skip") & filters.group)
+async def skip_cmd(client, message):
+    try:
+        await client.call_py.leave_group_call(message.chat.id)
+        await message.reply_text("⏭ Waa la gudbay. Qor /play hees kale.")
+    except Exception as e:
+        await message.reply_text(f"⚠️ Qalad: {e}")
