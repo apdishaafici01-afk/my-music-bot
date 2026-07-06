6a0aba1466b814f1f76ab72da80f783791d487fa
apdishaafici01-afk <apdishaafici01@gmail.com>
2026-07-06 18:37:26 +0300

    Update stop.py

--- plugin/stop.py
+++ plugin/stop.py
@@ -1,1 +1,9 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command("stop") & filters.group)
+async def stop_cmd(client, message):
+    try:
+        await client.call_py.leave_group_call(message.chat.id)
+        await message.reply_text("⏹ Dhammaan waa la joojiyay, Bot-ku VC-ga wuu ka baxay.")
+    except Exception as e:
+        await message.reply_text(f"⚠️ Qalad: {e}")
